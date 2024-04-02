dc651c88b875524f2322318a018f158c076cd470
Zastinian <zastinian@gmail.com>
2024-04-01 18:24:17 -0600

    feat: Typescript Code

--- Package/index.ts
+++ Package/index.ts
@@ -14,163 +14,159 @@ interface Table {
 interface QueueItem {
   method: string;
   table: string;
-  record?: { [key: string]: string };
-  query?: { [key: string]: string };
-  newData?: { [key: string]: string };
+  record?: {[key: string]: string};
+  query?: {[key: string]: string};
+  newData?: {[key: string]: string};
 }
 
-const createDatabase = (filePath: string, password: string) => {
-  let tables: { [key: string]: Table } = {};
-  const queue: QueueItem[] = [];
+const Database = class {
+  queue: QueueItem[] = [];
+  password: string;
+  filePath: string;
+  tables: {[key: string]: Table} = {};
+  constructor(filePath: string, password: string) {
+    this.tables = {};
+    this.filePath = filePath || "./database.ht";
+    this.password = password;
+    this.queue = [];
+  }
 
-  const saveToFile = () => {
-    if (!filePath.endsWith(".ht")) {
-      throw new Error(`File path must include '.ht': ${filePath}`);
-    }
-    const data = JSON.stringify(tables);
-    const encrypted = AES.encrypt(data, password).toString();
-    fs.writeFileSync(filePath, encrypted);
-  };
-
-  const readFromFile = () => {
-    if (!fs.existsSync(filePath)) {
-      return;
-    }
-    const encrypted = fs.readFileSync(filePath, "utf8");
-    const data = AES.decrypt(encrypted, password).toString(enc.Utf8);
-    try {
-      tables = JSON.parse(data);
-    } catch {
-      tables = {};
-    }
-  };
-
-  const processQueue = () => {
-    const request = queue[0];
-    switch (request.method) {
-      case "insert":
-        insertTable(request.table, request.record as { [key: string]: string });
-        break;
-      case "update":
-        updateTable(request.table, request.query as { [key: string]: string }, request.newData as { [key: string]: string });
-        break;
-      case "delete":
-        deleteFromTable(request.table, request.query as { [key: string]: string });
-        break;
-    }
-  };
-
-  const insertTable = (tableName: string, record: { [key: string]: string }) => {
-    readFromFile();
-    if (!tables[tableName]) {
-      throw new Error(`Table "${tableName}" does not exist.`);
-    }
-
-    const table = tables[tableName];
-    const formattedRecord = table.columns.reduce((obj, column) => ({ ...obj, [column]: record[column] || null }), {});
-    table.records.push(formattedRecord);
-    saveToFile();
-    queue.shift();
-    if (queue.length > 0) {
-      processQueue();
-    }
-  };
-
-  const updateTable = (tableName: string, query: { [key: string]: string }, newData: { [key: string]: string }) => {
-    readFromFile();
-    if (!tables[tableName]) {
-      throw new Error(`Table "${tableName}" does not exist.`);
-    }
-    const table = tables[tableName];
-    table.records = table.records.map((record) => {
-      if (Object.entries(query).every(([column, value]) => record[column] === value)) {
-        return { ...record, ...newData };
-      }
-      return record;
-    });
-    saveToFile();
-    queue.shift();
-    if (queue.length > 0) {
-      processQueue();
-    }
-  };
-
-  const deleteFromTable = (tableName: string, query: { [key: string]: string }) => {
-    readFromFile();
-    if (!tables[tableName]) {
-      throw new Error(`Table "${tableName}" does not exist.`);
-    }
-
-    tables[tableName].records = tables[tableName].records.filter(
-      (record) => !Object.entries(query).every(([column, value]) => record[column] === value),
-    );
-    saveToFile();
-    queue.shift();
-    if (queue.length > 0) {
-      processQueue();
-    }
-  };
-
-  const createTable = (tableName: string, columns: string[]): void => {
-    readFromFile();
-    if (tables[tableName]) {
+  createTable(tableName: string, columns: string[]): void {
+    this.readFromFile();
+    if (this.tables[tableName]) {
       throw new Error(`Table "${tableName}" already exists.`);
     }
 
-    tables[tableName] = { columns, records: [] };
-    saveToFile();
-  };
+    this.tables[tableName] = {columns, records: []};
+    this.saveToFile();
+  }
 
-  const deleteTable = (tableName: string) => {
-    readFromFile();
-    if (!tables[tableName]) {
+  deleteTable(tableName: string): void {
+    this.readFromFile();
+    if (!this.tables[tableName]) {
       throw new Error(`Table "${tableName}" does not exist.`);
     }
 
-    delete tables[tableName];
-    saveToFile();
-  };
+    delete this.tables[tableName];
+    this.saveToFile();
+  }
 
-  const insert = (tableName: string, record: { [key: string]: string }): void => {
-    queue.push({ method: "insert", table: tableName, record });
-    if (queue.length === 1) {
-      processQueue();
+  insert(tableName: string, record: {[key: string]: string}): void {
+    this.queue.push({method: "insert", table: tableName, record});
+    if (this.queue.length === 1) {
+      this.processQueue();
     }
-  };
+  }
 
-  const update = (tableName: string, query: { [key: string]: string }, newData: { [key: string]: string }): void => {
-    queue.push({ method: "update", table: tableName, query, newData });
-    if (queue.length === 1) {
-      processQueue();
+  update(tableName: string, query: {[key: string]: string}, newData: {[key: string]: string}): void {
+    this.queue.push({method: "update", table: tableName, query, newData});
+    if (this.queue.length === 1) {
+      this.processQueue();
     }
-  };
+  }
 
-  const select = (tableName: string, query: { [key: string]: string }): { [key: string]: string }[] => {
-    readFromFile();
-    if (!tables[tableName]) {
+  select(tableName: string, query: {[key: string]: string}): {[key: string]: string}[] {
+    this.readFromFile();
+    if (!this.tables[tableName]) {
       throw new Error(`Table "${tableName}" does not exist.`);
     }
-    if (!query) {
-      return tables[tableName].records;
-    }
-    return tables[tableName].records.filter((record) => Object.entries(query).every(([column, value]) => record[column] === value));
-  };
+    return this.tables[tableName].records.filter((record) => Object.entries(query).every(([column, value]) => record[column] === value));
+  }
 
-  const del = (tableName: string, query: { [key: string]: string }): void => {
-    queue.push({ method: "delete", table: tableName, query });
-    if (queue.length === 1) {
-      processQueue();
+  delete(tableName: string, query: {[key: string]: string}): void {
+    this.queue.push({method: "delete", table: tableName, query});
+    if (this.queue.length === 1) {
+      this.processQueue();
     }
-  };
+  }
 
-  return {
-    createTable,
-    deleteTable,
-    insert,
-    update,
-    select,
-    delete: del,
-  };
+  processQueue() {
+    const request = this.queue[0];
+    switch (request.method) {
+      case "insert":
+        this.insertTable(request.table, request.record as {[key: string]: string});
+        break;
+      case "update":
+        this.updateTable(request.table, request.query as {[key: string]: string}, request.newData as {[key: string]: string});
+        break;
+      case "delete":
+        this.deleteFromTable(request.table, request.query as {[key: string]: string});
+        break;
+    }
+  }
+
+  insertTable(tableName: string, record: {[key: string]: string}) {
+    this.readFromFile();
+    if (!this.tables[tableName]) {
+      throw new Error(`Table "${tableName}" does not exist.`);
+    }
+
+    const table = this.tables[tableName];
+    const formattedRecord = table.columns.reduce((obj, column) => ({...obj, [column]: record[column] || null}), {});
+    table.records.push(formattedRecord);
+    this.saveToFile();
+    this.queue.shift();
+    if (this.queue.length > 0) {
+      this.processQueue();
+    }
+  }
+
+  updateTable(tableName: string, query: {[key: string]: string}, newData: {[key: string]: string}) {
+    this.readFromFile();
+    if (!this.tables[tableName]) {
+      throw new Error(`Table "${tableName}" does not exist.`);
+    }
+    const table = this.tables[tableName];
+    table.records = table.records.map((record) => {
+      if (Object.entries(query).every(([column, value]) => record[column] === value)) {
+        return {...record, ...newData};
+      }
+      return record;
+    });
+    this.saveToFile();
+    this.queue.shift();
+    if (this.queue.length > 0) {
+      this.processQueue();
+    }
+  }
+
+  deleteFromTable(tableName: string, query: {[key: string]: string}) {
+    this.readFromFile();
+    if (!this.tables[tableName]) {
+      throw new Error(`Table "${tableName}" does not exist.`);
+    }
+
+    this.tables[tableName].records = this.tables[tableName].records.filter(
+      (record) => !Object.entries(query).every(([column, value]) => record[column] === value),
+    );
+    this.saveToFile();
+    this.queue.shift();
+    if (this.queue.length > 0) {
+      this.processQueue();
+    }
+  }
+
+  saveToFile() {
+    if (!this.filePath.endsWith(".ht")) {
+      throw new Error(`File path must include '.ht': ${this.filePath}`);
+    }
+    const data = JSON.stringify(this.tables);
+    const encrypted = AES.encrypt(data, this.password).toString();
+    fs.writeFileSync(this.filePath, encrypted);
+  }
+
+  readFromFile() {
+    if (!fs.existsSync(this.filePath)) {
+      return;
+    }
+    const encrypted = fs.readFileSync(this.filePath, "utf8");
+    const data = AES.decrypt(encrypted, this.password).toString(enc.Utf8);
+    try {
+      this.tables = JSON.parse(data);
+    } catch {
+      this.tables = {};
+    }
+  }
 };
 
-export default createDatabase;
+export default Database;
