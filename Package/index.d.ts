363e7e63cb967cb4f31ce8e2688a826a14733334
Zastinian <zastinian@gmail.com>
2024-04-01 18:24:24 -0600

    feat: Types

--- Package/index.d.ts
+++ Package/index.d.ts
@@ -1,21 +1,62 @@
-declare const createDatabase: (filePath: string, password: string) => {
-    createTable: (tableName: string, columns: string[]) => void;
-    deleteTable: (tableName: string) => void;
-    insert: (tableName: string, record: {
-        [key: string]: string;
-    }) => void;
-    update: (tableName: string, query: {
-        [key: string]: string;
-    }, newData: {
-        [key: string]: string;
-    }) => void;
-    select: (tableName: string, query: {
-        [key: string]: string;
-    }) => {
+interface Table {
+    columns: string[];
+    records: {
         [key: string]: string;
     }[];
-    delete: (tableName: string, query: {
+}
+interface QueueItem {
+    method: string;
+    table: string;
+    record?: {
         [key: string]: string;
-    }) => void;
+    };
+    query?: {
+        [key: string]: string;
+    };
+    newData?: {
+        [key: string]: string;
+    };
+}
+declare const Database: {
+    new (filePath: string, password: string): {
+        queue: QueueItem[];
+        password: string;
+        filePath: string;
+        tables: {
+            [key: string]: Table;
+        };
+        createTable(tableName: string, columns: string[]): void;
+        deleteTable(tableName: string): void;
+        insert(tableName: string, record: {
+            [key: string]: string;
+        }): void;
+        update(tableName: string, query: {
+            [key: string]: string;
+        }, newData: {
+            [key: string]: string;
+        }): void;
+        select(tableName: string, query: {
+            [key: string]: string;
+        }): {
+            [key: string]: string;
+        }[];
+        delete(tableName: string, query: {
+            [key: string]: string;
+        }): void;
+        processQueue(): void;
+        insertTable(tableName: string, record: {
+            [key: string]: string;
+        }): void;
+        updateTable(tableName: string, query: {
+            [key: string]: string;
+        }, newData: {
+            [key: string]: string;
+        }): void;
+        deleteFromTable(tableName: string, query: {
+            [key: string]: string;
+        }): void;
+        saveToFile(): void;
+        readFromFile(): void;
+    };
 };
-export default createDatabase;
+export default Database;
